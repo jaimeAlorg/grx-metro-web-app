babea34aecba101664897551bb71c71867099d50
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-02-02 11:43:08 +0100

    fix: General fixes to styling and implemented scroll to the top when clicking and item in the list

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -11,12 +11,39 @@ import { StationPageComponent } from "../station-page/station-page.component";
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent implements OnInit {
-  stationList: string[] = ['Station 1', 'Station 2', 'Station 3', 'Station 4', 'Station 5', 'Station 6', 'Station 7', 'Station 8', 'Station 9', 'Station 10'];
-  selectedStation: string = '';
+  stationList: string[] = [
+    'Albolote',
+    'Juncaril',
+    'Vicuña',
+    'Anfiteatro',
+    'Maracena',
+    'Cerrillo de Maracena',
+    'Jaén',
+    'Estación de autobuses',
+    'Argentinita',
+    'Luís Amador',
+    'Villarejo',
+    'Caleta',
+    'Estación de ferrocarril',
+    'Universidad',
+    'Méndez Núñez',
+    'Recogidas',
+    'Alcázar Genil',
+    'Hípica',
+    'Andrés Segovia',
+    'Palacio de deportes',
+    'Nuevo Los Cármenes',
+    'Dílar',
+    'Parque tecnológico',
+    'Sierra Nevada',
+    'Fernando de los Ríos',
+    'Armilla',
+  ];
+  selectedStation: string = 'Albolote';
   showStationData: boolean = false;
   isMobileView: boolean = false;
 
-  MOBILE_VIEW_WIDTH: number = 600;
+  MOBILE_VIEW_WIDTH: number = 750;
 
   ngOnInit(): void {
     this.checkViewport();
@@ -38,7 +65,11 @@ export class HomePageComponent implements OnInit {
   toggleStation(station: string): void {
     this.selectedStation = station;
     this.showStationData = true;
-    console.log('Selected station:', station, this.selectedStation);
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
 }
